7c9a96df9cb69e6f20f4e524b01e4414efaeb552
kimbackdoo <kdk7121743@naver.com>
2023-09-07 21:48:46 +0900

    feat: 테스트용 web 관련 코드 추가 및 nginx 코드 추가

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div>
+            <h1>Hello, Caddy Server</h1>
+            <h2>Current URL: {window.location.href}</h2>
+        </div>
+    )
+}
